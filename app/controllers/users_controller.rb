81633609b4a5bf09b5840bebe9fdf9452ed6ab52
Oliver Huang <oliverography@gmail.com>
2016-08-05 15:17:08 -0400

    Put js function script tag back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @user = User.find(params[:id])
-    
-    # if @user != current_user
-    #   redirect_to :back, :alert => "Access denied."
-    # end
+  def index
+    @user = current_user
   end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  
 end
